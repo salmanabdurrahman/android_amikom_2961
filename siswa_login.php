17ae2fbc987514db6365256c4f99b6937c5b6315
salmanabdurrahman <salmanabdurrahman12345@gmail.com>
2024-12-18 09:41:27 +0700

    fix: memperbaiki error

--- siswa_login.php
+++ siswa_login.php
@@ -2,27 +2,42 @@
 if (isset($_POST['kode']) && $_POST['kode'] == 'amikomoke') {
     include 'koneksi.php';
 
-    $nis = $_POST["nis"];
-    $password_siswa = sha1($_POST["password_siswa"]);
+    $nis = isset($_POST["nis"]) ? $_POST["nis"] : '';
+    $password_siswa = isset($_POST["password_siswa"]) ? $_POST["password_siswa"] : '';
 
-    // $db -> array();
+    if (empty($nis) || empty($password_siswa)) {
+        $out['hasil'] = "gagal";
+        $out['data'] = array();
+        echo json_encode($out);
+        exit;
+    }
+
+    $password_siswa = sha1($password_siswa);
+
+    // Hindari SQL Injection dengan menambahkan escape string
+    $nis = $db->real_escape_string($nis);
+    $password_siswa = $db->real_escape_string($password_siswa);
+
     $ambil = $db->query("SELECT * FROM siswa WHERE nis='$nis' AND password_siswa='$password_siswa'");
-    $siswa = $ambil->fetch_assoc();
 
-    if (empty($siswa)) {
+    if ($ambil === false) {
         $out['hasil'] = "gagal";
         $out['data'] = array();
     } else {
-        $out['hasil'] = "sukses";
-        $out['data'] = $materi;
-    }
+        $siswa = $ambil->fetch_assoc();
 
-    // while ($tiap = $ambil->fetch_assoc()) {
-    //     $materi[] = $tiap;
-    // }
+        if (empty($siswa)) {
+            $out['hasil'] = "gagal";
+            $out['data'] = array();
+        } else {
+            $out['hasil'] = "sukses";
+            $out['data'] = $siswa;
+        }
+    }
 } else {
     $out['hasil'] = "gagal";
     $out['data'] = array();
 }
 
 echo json_encode($out);
+?>
